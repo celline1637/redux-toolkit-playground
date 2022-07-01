6d7e8bd252182f6448efa5ef07ee6f137605f6a0
sol <sol@spoonradio.co>
2022-07-01 09:48:06 +0900

    Feature : 모달 내 전체 아이템 삭제 기능 추가

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,9 @@
 import { useAppDispatch, useAppSelector } from "../hooks/useRedux";
+import { clearCart } from "../redux/feature/cart/cartSlice";
 import { toggleModal } from "../redux/feature/modal/modalSlice";
 const Modal = () => {
   const dispatch = useAppDispatch();
+  const { amount } = useAppSelector((store) => store.cart);
   const { isOpen } = useAppSelector((store) => store.modal);
 
   const _toggleModal: React.MouseEventHandler = (e) => {
@@ -9,12 +11,18 @@ const Modal = () => {
     dispatch(toggleModal());
   };
 
+  const _clearCart: React.MouseEventHandler = (e) => {
+    if (amount === 0) return;
+    dispatch(clearCart());
+    _toggleModal(e);
+  };
+
   return isOpen ? (
-    <aside className="modal-container" onClick={(e) => _toggleModal(e)}>
+    <aside className="modal-container" onClick={_toggleModal}>
       <div className="modal">
         <h4>remove all items from your shopping cart? </h4>
         <div className="btn-container">
-          <button className="btn confirm-btn" onClick={_toggleModal}>
+          <button className="btn confirm-btn" onClick={_clearCart}>
             confirm
           </button>
           <button className="btn cancel-btn" onClick={_toggleModal}>
